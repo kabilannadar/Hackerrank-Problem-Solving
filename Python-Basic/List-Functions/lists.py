153d13bdac8243f0127f94e8bee233284450c336
Kabilan <r.r.kabilan0435@gmail.com>
2026-09-11 12:32:09 +0530

    solved command based list operations

---
+++ Python-Basic/List-Functions/lists.py
@@ -0,0 +1,20 @@
+N = int(input())
+data = []
+for i in range(N):
+    command = input().split(' ')
+    if command[0] == 'insert':
+        data.insert(int(command[1]), int(command[2]))
+    elif command[0] == 'print':
+        print(data)
+    elif command[0] == 'remove':
+        data.remove(int(command[1]))
+    elif command[0] == 'append':
+        data.append(int(command[1]))
+    elif command[0] == 'sort':
+        data.sort()
+    elif command[0] == 'pop':
+        data.pop()
+    elif command[0] == 'reverse':
+        data.reverse()
+    else:
+        pass
